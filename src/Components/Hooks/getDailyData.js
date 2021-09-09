e38c3b265788380797161328112241885a72d717
dialectrical <>
2021-09-09 16:28:11 -0700

    fixes bug in getDailyData API call

--- src/Components/Hooks/getDailyData.js
+++ src/Components/Hooks/getDailyData.js
@@ -1,10 +1,7 @@
-import { useEffect } from "react";
 import axios from "axios";
 import rateLimit from "axios-rate-limit";
 
 export const getDailyData = (time, url) => {
-  const hourStart = time[0];
-  const hourEnd = hourStart + 35400;
   const http = rateLimit(axios.create(), {
     maxRequests: 1,
     preMilliseconds: 1000,
@@ -14,13 +11,14 @@ export const getDailyData = (time, url) => {
 
   const fetchData = async x => {
     await http
-      .get(url + "&after=" + x + "&before=" + hourEnd)
+      .get(url + "&after=" + x + "&before=" + (x + 35400))
       .then(response => test.push(response.data.data.length));
   };
 
   const dailyData = async () => {
     for (let item of time) {
       await fetchData(item);
+      console.log(item);
       console.log(test);
     }
     return;
