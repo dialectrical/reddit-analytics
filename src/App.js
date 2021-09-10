ba05bc52203553477e57deedd5b38f15298afb05
dialectrical <>
2021-09-09 19:54:03 -0700

    displays heatmap numerical data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SubredditInput } from "./Components/SubredditInput";
 import { Heatmap } from "./Components/Heatmap";
+import { useGetWeeklyTimetable } from "./Components/Hooks/useGetWeeklyTimetable";
+import { getDailyData } from "./Components/Hooks/getDailyData";
 import { DisplayPosts } from "./Components/DisplayPosts";
 import { SubmitButton } from "./Components/SubmitButton";
 
 export const App = () => {
   const [redditData, setRedditData] = useState();
+  const [weeklyData, setWeeklyData] = useState();
+  const [activeHourlyData, setActiveHourlyData] = useState();
   const [subredditSearch, setSubredditSearch] = useState("fountainpens");
   const [baseURL, setBaseURL] = useState(
     "https://api.pushshift.io/reddit/search/submission/?subreddit=fountainpens&size=500"
   );
+  const weeklyTimetable = useGetWeeklyTimetable();
+
+  useEffect(() => {
+    getDailyData(weeklyTimetable, baseURL, setWeeklyData);
+  }, [baseURL]);
 
   const update = () => {
     setBaseURL(
@@ -17,13 +26,20 @@ export const App = () => {
         subredditSearch +
         "&size=500"
     );
+    setWeeklyData(null);
+    setActiveHourlyData(null);
   };
+
+  /* const showHourlyData = event => {
+    let list = makeHourlyList(event.currentTarget.value);
+    setActiveHourlyData(list);
+  }; */
 
   return (
     <div>
       {SubredditInput(setSubredditSearch)}
       {SubmitButton(update)}
-      {Heatmap(baseURL)}
+      {Heatmap(baseURL, weeklyData, setWeeklyData)}
       {redditData ? DisplayPosts(redditData) : <></>}
     </div>
   );
